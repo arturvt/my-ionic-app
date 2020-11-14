e7d780362a1a12d972573531e436fdda9395cbd7
Artur Tenorio <artur@ngti.nl>
2020-11-14 17:24:20 +0100

    fix loader

--- src/app/tab1/countries/detail/detail.component.ts
+++ src/app/tab1/countries/detail/detail.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CountryService } from '../country.service';
 import { CountryDetail, CountryDetailResponse } from '../country';
 import { ActivatedRoute } from '@angular/router';
-import { map, mergeMap } from 'rxjs/operators';
+import { finalize, map, mergeMap, take } from 'rxjs/operators';
 import { LoadingController } from '@ionic/angular';
 
 @Component({
@@ -11,26 +11,41 @@ import { LoadingController } from '@ionic/angular';
   styleUrls: ['./detail.component.scss'],
 })
 export class DetailComponent implements OnInit {
-
   countryDetail: CountryDetail;
   private loader: HTMLIonLoadingElement;
-  constructor(private route: ActivatedRoute, private countryService: CountryService, private loadingController: LoadingController) { }
-  ngOnInit() {
-    this.loadingController.create({
-      message: 'Please wait...'
-    }).then((val: HTMLIonLoadingElement) => {
-      this.loader = val;
-      this.loader.present();
-    });
+  constructor(
+    private route: ActivatedRoute,
+    private countryService: CountryService,
+    private loadingController: LoadingController
+  ) {}
+  ngOnInit(): void {
+    this.loadingController
+      .create({
+        message: 'Please wait...',
+      })
+      .then((val: HTMLIonLoadingElement) => {
+        this.loader = val;
+        this.loader.present();
+      });
 
-
-
-    this.route.paramMap.pipe(
-      mergeMap(params => this.countryService.getCountry(params.get('countryId'))),
-      map((response: CountryDetailResponse) => response.data)).subscribe((countryDetail: CountryDetail) => {
-      this.countryDetail = countryDetail;
-      this.loader.dismiss();
-    });
+    this.route.paramMap
+      .pipe(
+        take(1),
+        mergeMap((params) => this.countryService.getCountry(params.get('countryId'))),
+        map((response: CountryDetailResponse) => response.data),
+        finalize(() => this.loader.dismiss())
+      )
+      .subscribe(
+        (countryDetail: CountryDetail) => {
+          this.countryDetail = countryDetail;
+          console.log('?');
+        },
+        (error) => {
+          console.error(error);
+        },
+        () => {
+          console.log('on done');
+        }
+      );
   }
-
 }
