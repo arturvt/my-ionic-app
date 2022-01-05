3e8a11bd00da6211e692e09423042ea800eadf68
Artur Tenorio <Artur.Tenorio@swisscom.com>
2022-01-05 13:30:16 +0100

    updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { ActivatedRoute, Router, UrlSegment } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss']
+  styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private activatedRoute: ActivatedRoute,
+    private route: Router,
   ) {
     this.initializeApp();
+  }
+
+  ngOnInit(): void {
+    console.log('App component start');
+    this.activatedRoute.url.subscribe((urlSegment: UrlSegment[]) => {
+      const segments = urlSegment.map(
+        (urlSegment: UrlSegment) => urlSegment.path,
+      );
+      console.log('url!');
+      console.log(segments);
+    });
   }
 
   initializeApp() {
